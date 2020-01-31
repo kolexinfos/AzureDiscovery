352821a24a881b484f8aacd6363ab93f4886d3f1
Kehinde Kolade <kolexinfos@gmail.com>
2020-01-31 13:23:35 +0000

    Added PS for checking Extensions attached to VMs

---
+++ GetVMExtensions.ps1
@@ -0,0 +1,31 @@
+﻿$azSubs = Get-AzSubscription | Where-Object { $_.Name -Clike "*Internal*" }
+
+foreach ( $azSub in $azSubs ) {
+    
+    Write-Output $azSub
+
+    Set-AzContext -Subscription $azSub | Out-Null
+
+    $RGs = Get-AzResourceGroup
+
+    #foreach ( $rg in $RGs){
+        
+        
+        $VMs = Get-AzVM
+
+            foreach ($VM in $VMs) { 
+                try{
+                 $VMExt = Get-AzVMExtension -ResourceGroupName $VM.ResourceGroupName -VMName $VM.Name
+                 }
+                 catch{
+                   # Write-Host $_
+                 }            
+        
+                if($VMExt){
+                   Write-Output   "The Extensions for $($VM.Name) are $($VMExt.Name) "
+                }                
+     
+            } 
+    }
+
+#}
